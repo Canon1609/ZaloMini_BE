fc4bc8d66eff4ab025015d41a19a144b90424771
TRANQUOCANH\quoca <quocanh1609.iuh@gmail.com>
2025-05-05 10:54:48 +0700

    fix app auth

--- ZALO_MINI/routes/auth.routes.js
+++ ZALO_MINI/routes/auth.routes.js
@@ -4,8 +4,11 @@ const authController = require('../controllers/auth.controller');
 
 router.post('/register', authController.register);
 router.get('/verify-email', authController.verifyEmail);
+router.get('/verify-email-app', authController.verifyEmailApp);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
+router.post('/forgot-password-app', authController.forgotPasswordApp);
 router.post('/reset-password', authController.resetPassword);
+
 
 module.exports = router;
